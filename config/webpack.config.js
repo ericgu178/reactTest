40b6543318599dc218ad42492799717ddd64c111
EricGU178 <1143055543@qq.com>
2020-08-25 18:00:41 +0800

    优化加载页面

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -49,7 +49,8 @@ const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
-
+const CompressionPlugin = require("compression-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
 module.exports = function(webpackEnv) {
@@ -509,6 +510,14 @@ module.exports = function(webpackEnv) {
       ],
     },
     plugins: [
+        new CompressionPlugin({
+            filename: '[path].gz[query]', // 目标资源名称。[file] 会被替换成原资源。[path] 会被替换成原资源路径，[query] 替换成原查询字符串
+            algorithm: 'gzip', // 算法       
+            test: new RegExp('\\.(js|css)$'), // 压缩 js 与 css
+            threshold: 10240, // 只处理比这个值大的资源。按字节计算
+            minRatio: 0.8 // 只有压缩率比这个值小的资源才会被处理
+        }),
+        // new BundleAnalyzerPlugin(),
       // Generates an `index.html` file with the <script> injected.
       new HtmlWebpackPlugin(
         Object.assign(
