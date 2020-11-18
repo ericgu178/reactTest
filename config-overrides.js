3a53a3f6a929f75f26b214dfc15a4b3599dc5b44
EricGU178 <1143055543@qq.com>
2020-11-18 11:19:54 +0800

    cdn优化

--- config-overrides.js
+++ config-overrides.js
@@ -34,11 +34,17 @@ const meCustom = () => config => {
     //     }
     // };
 
+    // 外部扩展 「从输出的 bundle 中排除依赖」的方法。
+    // 相反，所创建的 bundle 依赖于那些存在于用户环境(consumer's environment)中的依赖。
+    // 此功能通常对 library 开发人员来说是最有用的，然而也会有各种各样的应用程序用到它。
+
     config.externals = {
         react: 'React',
         'react-dom': 'ReactDOM',
+        'react-router-dom': 'ReactRouterDOM',
         antd:'antd',
-        'highlight.js':'hljs'
+        'highlight.js':'hljs',
+        marked: 'marked'
     }
 
     config.plugins.push(new CompressionPlugin({
